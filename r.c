69a50a63223fbea37e0c2062aaaca80e2bc538f2
asherlie <lieber.asher@gmail.com>
2023-08-27 23:20:26 -0400

    ctrl-c now safely exits implemented lookup_ip_bucket(), p_stream() added p_stream_thread and select_ip_thread

--- r.c
+++ r.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <signal.h>
 #include <stdatomic.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
@@ -13,6 +16,8 @@
 
 #define ANSI_CLR "\033[2J"
 #define ANSI_HOM "\033[H"
+
+volatile _Bool run = 1;
 
 struct ps_filter{
     uint8_t* s_addr;
@@ -50,11 +55,23 @@ struct packet_storage{
     int n_packets;
     struct ip_bucket** buckets;
 
+    /* below fields are used for selecting and printing specific ip addresses */
     _Atomic int idx_filter;
     _Atomic int n_ips, ip_cap;
     _Atomic in_addr_t* ip_addresses;
     _Atomic in_addr_t* old_ptr;
+
+    /* TODO: store bucket IP so we can print a header on first packet
+     * of new bucket
+     */
+    /*char bucket_ip[18];*/
+    _Atomic (struct address_packets*) ap_print;
 };
+
+void exit_loop(int sig){
+    (void)sig;
+    run = 0;
+}
 
 void init_packet_storage(struct packet_storage* ps, int n_buckets){
     ps->n_buckets = n_buckets;
@@ -124,6 +141,23 @@ void insert_ip_list(struct packet_storage* ps, struct packet* p){
     }
     ps->ip_addresses[ps->n_ips] = p->ihdr.saddr;
     atomic_store(&ps->n_ips, ps->n_ips+1);
+}
+
+/* TODO: insertion isn't threadsafe, there's a chance that we could be
+ * reading from an invalid head
+ * TODO: update pointer updates to be atomic in insert_packet_storage()
+ * this isn't a huge deal though because buckets don't change once initialized
+ */
+struct ip_bucket* lookup_ip_bucket(struct packet_storage* ps, in_addr_t addr){
+    int idx = addr % ps->n_buckets;
+    struct ip_bucket* ib;
+
+    for(ib = ps->buckets[idx]; ib; ib = ib->next){
+        if(ib->head->p->ihdr.saddr == addr){
+            return ib;
+        }
+    }
+    return NULL;
 }
 
 struct address_packets* insert_packet_storage(struct packet_storage* ps, struct packet* p, ssize_t sz){
@@ -286,7 +320,13 @@ _Bool filter_packet(uint8_t* p, ssize_t br, uint8_t* s_addr, uint8_t* d_addr, ch
     return 1;
 }
 
-void p_ip_addresses(struct packet_storage* ps){
+/*
+ * void select_ip(struct packet_storage* ps, in_addr_t ip){
+ *     
+ * }
+*/
+
+void ip_addresses_op(struct packet_storage* ps, int sel_idx){
     char ipbuf[18] = {0};
     int n_ips = atomic_load(&ps->n_ips);
     /* there's a chance that this has been realloc'd
@@ -296,11 +336,29 @@ void p_ip_addresses(struct packet_storage* ps){
      * after resizing of buffer anyway
      */
     _Atomic in_addr_t* addresses = atomic_load(&ps->ip_addresses);
+    // TODO:
+    // atomic_compare_exchange_strong() should really be used here
+    // to avoid double free()ing
+    // there's a possibility that two threads call this function simultaneously
+    // and both load old_ptr and try to free it
+    // we should CAS() to update old_ptr to NULL once we read it
     _Atomic in_addr_t* old_ptr = atomic_load(&ps->old_ptr);
 
-    for(int i = 0; i < n_ips; ++i){
-        iaddr_to_str(addresses[i], ipbuf, sizeof(ipbuf));
-        printf("(%i) - IP %s\n", i, ipbuf);
+    if(sel_idx >= 0 && sel_idx < n_ips){
+        /*_Atomic struct ip_bucket* aib = lookup_ip_bucket(ps, addresses[sel_idx]);*/
+        /*atomic_store(&ps->ap_print, aib);*/
+        // TODO: may need to change ib->head to be _Atomic
+        atomic_store(&ps->ap_print, lookup_ip_bucket(ps, addresses[sel_idx])->head);
+    }
+    else{
+        /* TODO: is this good behavior?
+         * do we want to nullify ap_print when a bad idx is passed?
+         */
+        atomic_store(&ps->ap_print, NULL);
+        for(int i = 0; i < n_ips; ++i){
+            iaddr_to_str(addresses[i], ipbuf, sizeof(ipbuf));
+            printf("(%i) - IP %s\n", i, ipbuf);
+        }
     }
 
     if(old_ptr){
@@ -308,6 +366,16 @@ void p_ip_addresses(struct packet_storage* ps){
         atomic_store(&ps->old_ptr, NULL);
     }
 }
+
+void p_ip_addresses(struct packet_storage* ps){
+    ip_addresses_op(ps, -1);
+}
+
+/*
+ * void p_packet_storage_exp(struct packet_storage* ps){
+ *     _Atomic struct 
+ * }
+*/
 
 // prints out all packets for a given user according to the given idx
 // actually, this should take in in_addr_t and be CALLED by p_packet_storage()
@@ -342,6 +410,26 @@ void p_packet_storage(struct packet_storage* ps, _Bool summary, struct ps_filter
 }
 
 /*
+    atomic load ap_print
+    return 0 !ap_print
+then at the end, we can
+    CAS(ap_print, ap_print->next)!
+
+this way, if it's been NULL-ified by an idx of -1 being set
+we won't set it to a weird position in the middle of a bucket
+when we shouldn't be printing anything
+
+*/
+_Bool p_stream(struct packet_storage* ps){
+    /*atomic-l*/
+    struct address_packets* ap = atomic_load(&ps->ap_print);
+    if(!ap)return 0;
+    p_packet(ap, 1);
+    atomic_compare_exchange_strong(&ps->ap_print, &ap, ap->next);
+    return 1;
+}
+
+/*
  * read from stdin, any time enter is pressed
  * we set the filter to the corresponding index
  *
@@ -366,6 +454,7 @@ void p_packet_storage(struct packet_storage* ps, _Bool summary, struct ps_filter
 */
 
 /*
+ * TODO:
  * need:
  *  a thread to populate packet_storage
  *      what's currently done in main()
@@ -380,9 +469,66 @@ void p_packet_storage(struct packet_storage* ps, _Bool summary, struct ps_filter
  *      new print index is read (set ptr = bucktes[idx][ip][0])
  *      we print a packet (increment ptr)
  *
- * this is a great design but we still need to keep track of IP indices to reference them
+ * TODO: print the number of packets next to IP when outputted from ip_addresses
 */
-/*void* */
+void* p_stream_th(void* arg){
+    struct packet_storage* ps = arg;
+
+    while(run){
+        /*
+         * this logic is bad because even if we haven't had a packet in a while
+         * we need to stay waiting to stream
+         * we can do this by checking the ip of ps?
+        */
+        // TODO:
+        // maybe we can only print once there's a new IP and we won't need to clear screen
+        // this won't corrupt user input either
+        // can also probably use this logic to not print guide again after we're out of packets from a given IP
+        // HERE:
+        //  if our last actual print was a real packet, then WAIT!
+        //  keep printing nothing at all
+        //  make sure that 
+        //  the issue is that streaming won't work with this setup because new packets within
+        //  a bucket won't be added to ps->ap_print->next
+        //  we can only print packets after they've been processed
+        //
+        //  we could potentially also keep track of ip integer and check when we get a new packet
+        //  if ip int is valid
+        //  if(ip_int == current_ip)
+        //      CAS(ap_print, NULL, new_ap);
+        //  but this could cause problems if we:
+        //      select idx
+        //      print all packets
+        //      user sets idx to -1, ap_print is set to NULL
+        //      we get a new packet
+        //      ap_print is overwritten with new packet
+        //
+        //  this could be countered by having a flag that's set when the user sets it to NULL
+        //
+        //  THIS IS WHAT THE NEXT STEP IS
+        //  maybe we could store prev_ap each time we print!
+        //  if NULL, print prev_ap->next, repeat
+        if(!p_stream(ps)){
+            printf("%s%s", ANSI_CLR, ANSI_HOM);
+            p_ip_addresses(ps);
+        }
+        usleep(1000000);
+    }
+    return NULL;
+}
+
+void* select_ip_th(void* arg){
+    struct packet_storage* ps = arg;
+    int idx;
+
+    while(run){
+        /* TODO: don't use fscanf */
+        if(!fscanf(stdin, "%i", &idx))continue;
+        ip_addresses_op(ps, idx);
+        /*printf("SET IDX TO %i\n", idx);*/
+    }
+    return NULL;
+}
 
 /*
  * TODO:
@@ -405,13 +551,18 @@ int main(){
     ssize_t br;
     struct packet_storage ps;
     struct address_packets* ap;
+    pthread_t p_stream_pth, select_ip_pth;
+
+    signal(SIGINT, exit_loop);
 
     init_packet_storage(&ps, 10000);
     
+    pthread_create(&p_stream_pth, NULL, p_stream_th, &ps);
+    pthread_create(&select_ip_pth, NULL, select_ip_th, &ps);
 
     /*printf("sock %i\n", sock);*/
     /*br = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr*)&sa_i, &slen_i);*/
-    while(1){
+    while(run){
         buf = read_packet(sock, 4096, &br);
 
         if(!buf)continue;
@@ -423,14 +574,32 @@ int main(){
             free(buf);
             continue;
         }
-        puts("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        printf("read %li bytes\n", br);
-        printf("%s%s", ANSI_CLR, ANSI_HOM);
-        p_ip_addresses(&ps);
+        /* if we've inserted a packet, p_stream()
+         * there's a chance the summary has changed
+         * TODO: only p_stream() if we have a new address or ap->p->ihdr.saddr == 
+         */
+        /*p_stream()*/
+        /*
+         * puts("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+         * printf("read %li bytes\n", br);
+        */
+        /*printf("%s%s", ANSI_CLR, ANSI_HOM);*/
+        /*p_ip_addresses(&ps);*/
+
+        /*
+        if(!p_stream(&ps)){
+            printf("%s%s", ANSI_CLR, ANSI_HOM);
+            p_ip_addresses(&ps);
+        }
+        */
+
         /*p_packet_storage(&ps, 1, NULL);*/
 
         (void)ap;
         /*p_packet(ap, 1);*/
         /*free(buf);*/
     }
+    pthread_join(p_stream_pth, NULL);
+    kill(getpid(), SIGUSR1);
+    pthread_join(select_ip_pth, NULL);
 }
